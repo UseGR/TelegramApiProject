3af139e8dbf1c55d21293da8e302b0ab79b9a155
RusОфJavaDev <r.galeev116@gmail.com>
2023-02-17 21:52:02 +0300

    configuration rabbitmq and run in docker

--- dispatcher/src/main/java/ru/galeev/service/impl/AnswerConsumerImpl.java
+++ dispatcher/src/main/java/ru/galeev/service/impl/AnswerConsumerImpl.java
@@ -1,16 +1,21 @@
 package ru.galeev.service.impl;
 
+import lombok.Data;
 import lombok.extern.log4j.Log4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.galeev.service.UpdateProducer;
 
 @Service
 @Log4j
+@Data
 public class AnswerConsumerImpl implements UpdateProducer {
+    private final RabbitTemplate rabbitTemplate;
 
     @Override
     public void produce(String rabbitQueue, Update update) {
         log.debug(update.getMessage().getText());
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 }
